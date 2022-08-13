812e7f9d65e57c407cb0a6571928a22b2037e490
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-13 11:05:32 +1000

    Help from David Impey - (review session) discussed many different ways of doing async/promise. I will continue to use his methodology as a way of learning how to use techniques I hadn't tried even though he laid the framework

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,27 +1,66 @@
 const choosePath = require("../inquirerRoutes/choosePath");
 
-const mysql = require('mysql2');
-const db = require('../config/connection');
+// const mysql = require('mysql2');
+const getConnection = require('../config/connection');
 
 
 const inquirerRouter = require('../inquirerRoutes/index');
 const console_table = require('console.table');
 
-const viewAll = async () => {
+const viewAll = async (table) =>  {
     try {
-        const allData =  await db.query('SELECT * FROM employee', function(error,result){
-            return result;
-        });
+        const db = await getConnection();
+        
+        const [result] = await db.query(`SELECT * FROM ${table}`);
 
-        //const allData =  await db.query('SELECT * FROM employee');
-        //console.log(allData);
-        // choosePath()
+        return result;       
        
     } catch (error) {
         console.log(error);
     }
 
 }
+
+// const viewAll = (table) =>  {
+//     try {
+//         return getConnection().then((db) => {
+//             return db.query(`SELECT * FROM ${table}`);
+//         }).then(([result]) => {
+//             return result;
+//         })
+        
+
+//         // return result;
+
+//         //const allData =  await db.query('SELECT * FROM employee');
+//         //console.log(allData);
+//         // choosePath()
+       
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+
+// const viewAll = (table) => new Promise((resolve, reject) => {
+//     try {
+//         const allData = db.query(`SELECT * FROM ${table}`, function(error, result){
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve(result);
+//             }
+//         });
+
+//         //const allData =  await db.query('SELECT * FROM employee');
+//         //console.log(allData);
+//         // choosePath()
+       
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// });
 
 
 // viewAll(table, async (req, res) => {
@@ -54,8 +93,13 @@ const viewAll = ((table) => {
 })
 */
 
+const viewOne = () => {
+    
+}
+
 
 // Module Exports
 module.exports = {
-    viewAll
+    viewAll,
+    viewOne
 }
